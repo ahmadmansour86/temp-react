d023cd0d59826fe40f9a7e8090e77ad40bdc871c
ahmadmansour86 <57324058+ahmadmansour86@users.noreply.github.com>
2019-12-11 12:46:09 +0100

    Updates

---
+++ precache-manifest.dc41f35da4730892a47bc0ece9ad510e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a58904c1309f98c798c05dfc0b8513e8",
+    "url": "/temp-react/index.html"
+  },
+  {
+    "revision": "691b03d821eaa60f8403",
+    "url": "/temp-react/static/css/main.b52cc451.chunk.css"
+  },
+  {
+    "revision": "94398fb3a1b9348dcf95",
+    "url": "/temp-react/static/js/2.c65c5976.chunk.js"
+  },
+  {
+    "revision": "691b03d821eaa60f8403",
+    "url": "/temp-react/static/js/main.ff428eca.chunk.js"
+  },
+  {
+    "revision": "13399ab00c5ce880732c",
+    "url": "/temp-react/static/js/runtime-main.36dfef5e.js"
+  }
+]);
